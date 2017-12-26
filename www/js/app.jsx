13740b37e25e285fa88c91358f533b688c5e49cf
Kamil Jiwa <kamil.jiwa@gmail.com>
2017-12-25 21:28:43 -0800

    Add a home route component.

--- www/js/app.jsx
+++ www/js/app.jsx
@@ -1,11 +1,12 @@
 import '../css/uwsolar.css';
 
+import HomeRoute from './home-route.jsx';
 import React from 'react';
 import ReactDOM from 'react-dom';
 
 class App extends React.Component {
   render() {
-    return <p>Hello</p>;
+    return <HomeRoute />;
   }
 }
 
---
+++ www/js/home-route.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export class HomeRoute extends React.Component {
+  render() {
+    return (
+      <div>
+        <div>Header</div>
+        <div>Body</div>
+        <div>Footer</div>
+      </div>
+    );
+  }
+}
+
+export default HomeRoute;
