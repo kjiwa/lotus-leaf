7abccceac94a6380d9a298af0e701747e9aa6638
Kamil Jiwa <kamil.jiwa@gmail.com>
2017-12-25 21:34:10 -0800

    Add react-router and a route for the home component.

--- www/js/app.jsx
+++ www/js/app.jsx
@@ -3,10 +3,15 @@ import '../css/uwsolar.css';
 import HomeRoute from './home-route.jsx';
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter, Route } from 'react-router-dom';
 
 class App extends React.Component {
   render() {
-    return <HomeRoute />;
+    return (
+      <BrowserRouter>
+        <Route exact path="/" component={HomeRoute} />
+      </BrowserRouter>
+    );
   }
 }
 
